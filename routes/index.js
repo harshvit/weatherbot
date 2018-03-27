626e22eaf4225465fc69b652d2ff1e7a847104f8
hmharshith <harshith.hm@thedatateam.in>
2018-03-27 15:09:08 +0530

    post request

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 
 app.post("/", function (req, res) {
-    res.send("Hello ! , Welcome to weatherBot");
-})
+    res.setHeader('Content-Type', 'application/json');
 
-app.listen(process.env.PORT||3000 , function(){
-    console.log(JSON.stringify({
+    res.send(JSON.stringify({
         "response" : "Started WeatherBot server at port 3000" ,
         "error" : null
     }));
+})
+
+app.listen(process.env.PORT||3000 , function(){
+    console.log("Started server !");
 });
