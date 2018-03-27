27cd43c7bbd0d0b757d654080006ea0b0158420a
hmharshith <harshith.hm@thedatateam.in>
2018-03-27 15:37:39 +0530

    post request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
+var temp = 32
+var type = "Sunny"
+
 app.post("/", function (req, res) {
 
-    responseTxt = "Welcome to WeatherBot";
+    responseTxt = "Welcome to WeatherBot! Weather in " + req.body.result.parameters['geo-city'] + " is " + type +
+                    ". (Temperature: " + temp +")";
+    
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({
         "speech" : responseTxt ,
