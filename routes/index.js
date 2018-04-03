6eee30c7142d57c071d88ec92fb8cdddae7f9de9
harshvit <hmharshith@gmail.com>
2018-04-03 18:00:46 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const bodyParse = require("body-parser");
 // const tools = require("../public/javascripts/getPropertyTools");
 const fs = require('fs');
-const fireBase = require("firebase");
 var router = express.Router();
 const app = express();
 app.use(bodyParse.json());
 
-var db = tools.getFireStore()
 
 app.post("/", function (req, res) {
         res.send(JSON.stringify("{error: null}"));
